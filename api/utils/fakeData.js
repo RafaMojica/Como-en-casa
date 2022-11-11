176c1bb8600903534ac05fb648da9261553038dc
lucas h <lucas.ph@live.com>
2022-11-11 13:44:56 -0300

    se modifica ext. y se agrga const. para exportar

---
+++ api/utils/fakeData.js
@@ -0,0 +1,28 @@
+const users = [
+  {
+    nombre: "como en casa",
+    direccion: "San Martin 123",
+    telefono: "3513959595",
+    contraseña: "1234",
+    email: "superadmin@admin.com",
+    superAdmin: true,
+    tipo: true,
+  },
+  {
+    nombre: "Pepe",
+    direccion: "San Martin 123",
+    telefono: "3513959595",
+    contraseña: "1234",
+    email: "pepe@argento.com",
+  },
+  {
+    nombre: "Moni",
+    direccion: "San Martin 123",
+    telefono: "3513959595",
+    contraseña: "1234",
+    email: "moni@argento.com",
+    tipo: true,
+  },
+];
+
+module.exports = users;
